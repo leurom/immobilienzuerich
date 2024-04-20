e3846cb369768d613ea9f4ccb107e2a6f189c1fd
leurom <leu.roman@bluewin.ch>
2024-04-20 15:48:54 +0200

    map

--- static/main.js
+++ static/main.js
@@ -5,7 +5,43 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// Marker für ausgewählten Standort
+var marker = L.marker([47.3667, 8.5500], { draggable: true }).addTo(map);
+
+// Event-Handler für das Verschieben des Markers
+marker.on('dragend', function(event){
+    var position = marker.getLatLng(); // Aktuelle Position des Markers
+    console.log('Neue Position:', position.lat, position.lng); // Ausgabe der Koordinaten in der Konsole
+    // Hier kannst du die Koordinaten weiterverarbeiten, z.B. in ein Formular eintragen
+});
+
+
 // Optional: Marker für Zürich hinzufügen
 /* L.marker([47.3667, 8.5500]).addTo(map)
     .bindPopup('Zürich Canton')
     .openPopup(); */
+
+/*
+var map = L.map('map').setView([47.3667, 8.5500], 10); // Koordinaten für Zürich, Zoom-Level 10
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Laden und Anzeigen der GeoJSON-Grenzdaten des Kantons Zürich
+fetch('/static/map.geojson')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        L.geoJSON(data, {
+            style: function(feature) {
+                return {
+                    color: 'blue', // Farbe der Grenzlinie
+                    weight: 2, // Dicke der Linie
+                    opacity: 1 // Deckkraft der Linie
+                };
+            }
+        }).addTo(map);
+    });
+*/
